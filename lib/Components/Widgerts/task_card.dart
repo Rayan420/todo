95c6be50afcc182b3a053b8089dd34385acc12ea
Rayan420 <ahmed.rayan@st.uskudar.edu.tr>
2022-10-11 16:16:43 +0300

    current version of the app

--- lib/Components/Widgerts/task_card.dart
+++ lib/Components/Widgerts/task_card.dart
@@ -1,44 +1,61 @@
 import 'package:flutter/material.dart';
 
 class TaskCard extends StatelessWidget {
-  const TaskCard({Key? key, required this.color,required this.title,required this.desc}) : super(key: key);
+  const TaskCard(
+      {Key? key, required this.color, required this.title, required this.desc})
+      : super(key: key);
 
   final Color color;
   final String? title;
   final String? desc;
+
   @override
   Widget build(BuildContext context) {
     return Container(
       width: 200,
       height: 200,
-      padding: EdgeInsets.symmetric(
+      padding: const EdgeInsets.symmetric(
         vertical: 32.0,
         horizontal: 24.0,
       ),
-      margin: EdgeInsets.only(right: 20),
+      margin: const EdgeInsets.only(right: 20),
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(20),
         color: color,
       ),
-      child: Column(
+      child: Stack(
         children: [
-          Text(
-            title?? "Unnamed Task",
-            style: TextStyle(
-              fontWeight: FontWeight.bold,
-              fontSize: 19,
+          // icon button in stack to remove a unwanted task list
+          Positioned(
+            bottom: -14.0,
+            right: -14.0,
+            child: IconButton(
+              onPressed: null,
+              icon: Icon(Icons.remove_circle_outline, color: Colors.black,),
             ),
           ),
-          Padding(
-            padding: EdgeInsets.only(top: 10),
+          Column(
+            children: [
+              Text(
+                title ?? "Unnamed Task",
+                style: const TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 19,
+                ),
+              ),
+              const Padding(
+                padding: EdgeInsets.only(top: 10),
+              ),
+              Text(
+                desc ?? "No Description",
+                style: const TextStyle(
+                  height: 1.5,
+                  fontSize: 16,
+                ),
+              )
+            ],
           ),
-          Text(
-            desc?? "No Description",
-            style: TextStyle(
-              height: 1.5,
-              fontSize: 16,
-            ),
-          )
+
         ],
       ),
     );
